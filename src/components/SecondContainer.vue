b86e42e8fd834e582cd3da76ba33a083268a63b3
farhanzeb95 <farhanzeb95@gmail.com>
2020-02-16 16:14:25 +0100

    added second container

--- src/components/SecondContainer.vue
+++ src/components/SecondContainer.vue
@@ -1,6 +1,53 @@
 <template>
   <div class="secondContainer">
+    <div>
     <h2 class="topText">Dein Testpaket enthält</h2>
+  </div>
+    <div class="subContainer1">
+     <div style="float:left; width:30%">
+       <img src="../images/lillydoo-little-blowballs-design-preview-tp.jpg">
+     </div>
+
+          <div style="float: right;width:70%">
+       <h4>10 Lillydoo Windeln</h4>
+       <ul>
+         <li>
+           0 % Parfüme & Lotionen, 100 % LILLYDOO Schutz
+         </li>
+
+         <li>
+            Extra weich und super bequem
+         </li>
+
+         <li>
+            Mit unseren Turbo Tunneln für eine ideale Passform und schnelle Absorption
+         </li>
+       </ul>
+      </div>
+    </div>
+
+    <div class="subContainer2">
+      <div style="float:left; width:40%">
+        <img src="../images/water-wipes-15-small.jpg">
+      </div>
+
+      <div style="float: right; width:60%">
+      <h4>
+        15 Feuchttücher mit 99 % Wasser
+      </h4>
+
+      <ul>
+        <li>
+          0 % Parfüme & PEGs, 100 % biologisch abbaubar
+        </li>
+
+        <li>
+          Natürlich rein, extra mild, Alternative zu "Wasser & Watte"
+        </li>
+      </ul>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -10,8 +57,9 @@
 
 <style scoped lang="scss">
 .secondContainer {
-    margin-top: 20px;
+    margin-top: 30px;
     height: auto;
+    // border: 2px solid green;
 }
 .topText{
    font-family: cera_proregular,Helvetica,Roboto,Arial,sans-serif;
@@ -22,5 +70,19 @@
     margin-bottom: 10px;
     font-weight: normal;
     text-align: center !important;
+}
+
+.subContainer1{
+  width: 49%;
+  // border: 2px solid black;
+  float: left;
+  height: auto;
+}
+
+.subContainer2{
+  width: 49%;
+  // border: 2px solid red;
+  float: right;
+  height: auto;
 }
 </style>
